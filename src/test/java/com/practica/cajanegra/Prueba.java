c5d323e78dd0fa371bb54feff6a76edadc1998f6
jcanterom <60644476+jcanterom@users.noreply.github.com>
2021-11-15 23:41:45 +0100

    Creación clase Prueba

--- src/test/java/com/practica/cajanegra/Prueba.java
+++ src/test/java/com/practica/cajanegra/Prueba.java
@@ -2,11 +2,13 @@ package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Prueba {
     @Test
+    @DisplayName("Prueba ejemplo inicial")
     public void miTestDeEjemplo(){
         SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B");
         assertEquals(2,mylist.size());
