abc9146b0988ac0985fa74c5f83d2cf2ad1bff54
Aggpm <a.garrudo@alumnos.upm.es>
2021-12-12 23:19:22 +0100

    actualizacion nombreClases

--- src/test/java/com/practica/cajanegra/AddTest.java
+++ src/test/java/com/practica/cajanegra/AddTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class Add {
+public class AddTest {
 
     private SingleLinkedListImpl<String> mylist;
     private SingleLinkedListImpl<String> listvacia;
