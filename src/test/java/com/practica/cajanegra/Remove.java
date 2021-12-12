5b1098f1b68272f717ed11d3e3c670c312da51fa
bp0254_bo0489 <bp0254_bo0489@outlook.es>
2021-12-12 20:34:30 +0100

    Pruebas de los dos métodos de removeLast.

--- src/test/java/com/practica/cajanegra/Remove.java
+++ src/test/java/com/practica/cajanegra/Remove.java
@@ -1,21 +1,63 @@
 package com.practica.cajanegra;
 
+import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.SingleLinkedList;
 import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Remove {
 
-    SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<>("A", "B", "C", "D", "E",
-            "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-            "Y", "Z");
-    SingleLinkedListImpl<String> badlist = new SingleLinkedListImpl<>("@", "B", "C", "D", "E",
-            "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "o", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-            "Y", "1");
-    SingleLinkedListImpl<String> emptyList = new SingleLinkedListImpl<>();
-    SingleLinkedListImpl<String> list = new SingleLinkedListImpl<>("A");
-    SingleLinkedListImpl<String> list1 = new SingleLinkedListImpl<>("A", "B");
+    SingleLinkedListImpl<String>mylist = new SingleLinkedListImpl<>("A", "B", "M", "Y", "Z");
+    SingleLinkedListImpl<String>badlist = new SingleLinkedListImpl<>("A", "B", "@", "[", "Z");
+    SingleLinkedListImpl<String>emptyList = new SingleLinkedListImpl<>();
 
+    @BeforeEach
+    public void iniciarLista(){
+        mylist = new SingleLinkedListImpl<String>("A", "B", "M", "Y", "Z");
+    }
+
+    @ParameterizedTest(name = "Elimina el ultimo elemento de la lista")
+    @ValueSource( strings ={"A","B","M","Y","Z"})
+    public void testRemoveLastValid() throws EmptyCollectionException {
+        mylist.removeLast();
+        assertEquals("[A, B, M, Y]",mylist.toString());
+    }
+
+    @ParameterizedTest(name = "Elimina el ultimo elemento de la lista")
+    @ValueSource( strings ={"A","B","M","Y","Z"})
+    public void testRemoveLastInvalid() throws EmptyCollectionException {
+        emptyList.removeLast();
+        assertEquals("[]",emptyList.toString());
+    }
+
+    /*--------------------------Test RemoveLast (String elem)-----------------------*/
+    @ParameterizedTest(name = "Elimina la última aparición de {0}")
+    @CsvSource(value = {
+            "A:  [B, M, Y, Z]",
+            "B:  [A, M, Y, Z]",
+            "M:  [A, B, Y, Z]",
+            "Y:  [A, B, M, Z]",
+            "Z:  [A, B, M, Y]",
+    }, delimiter=':')
+
+    public void testRemoveLastValid(String elem, String expected) throws NoSuchElementException, EmptyCollectionException {
+        mylist.removeLast(elem);
+        assertEquals(expected,mylist.toString());
+    }
+
+    @ParameterizedTest(name = "Error al eliminar el elemento no valido {0}")
+    @ValueSource( strings ={"@","["})
+    public void testRemoveLastInvalid(String elem) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            badlist.removeLast(elem);
+        });
+    }
 
 }
+
